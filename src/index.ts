29f620554ebd40f30cdd292de5c74719082b48ae
Yuriy Rypka <y.rypka@relevant.software>
2019-11-27 19:02:49 +0200

    setup caching

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,35 @@
-import * as hapi from '@hapi/hapi';
+// import * as Hapi from '@hapi/hapi';
+// import CatboxRedis from '@hapi/catbox-redis';
+const Hapi = require('@hapi/hapi');
+const CatboxRedis = require('@hapi/catbox-redis');
 
 console.log(`Running environment ${process.env.NODE_ENV || 'dev'}`);
 
-// Catch unhandling unexpected exceptions
+// Catch uncaught exceptions
 process.on('uncaughtException', (error: Error) => {
   console.error(`uncaughtException ${error.message}`);
 });
 
-// Catch unhandling rejected promises
+// Catch unhandled rejected promises
 process.on('unhandledRejection', (reason: any) => {
   console.error(`unhandledRejection ${reason}`);
+  console.error({ reason });
 });
 
 const init = async () => {
-  const server = new hapi.Server({
+  const server = new Hapi.server({
     host: 'localhost',
     port: 8000,
+    cache: {
+      name: 'redis-cache',
+      provider: {
+        constructor: CatboxRedis,
+        options: {
+          partition: 'my_cached_data',
+          tls: {},
+        },
+      },
+    },
   });
 
   await server.start();
